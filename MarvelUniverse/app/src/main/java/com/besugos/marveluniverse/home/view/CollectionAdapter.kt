25ea6b4a86bae90996f0c9a6163c921dc697cb51
gabrielaraujoz <garaujo.eng@gmail.com>
2020-11-21 20:06:50 -0300

    Criação dos Fragments e vinculação no ViewPager2, it's working!

--- MarvelUniverse/app/src/main/java/com/besugos/marveluniverse/home/view/CollectionAdapter.kt
+++ MarvelUniverse/app/src/main/java/com/besugos/marveluniverse/home/view/CollectionAdapter.kt
@@ -1,16 +1,30 @@
 package com.besugos.marveluniverse.home.view
 
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class CollectionAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
+class CollectionAdapter(private val fragment: Fragment): FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int = 4
 
     override fun createFragment(position: Int): Fragment {
-        val fragment = DemoFragment()
-        fragment.arguments = Bundle().apply {
-            putInt("ARG_OBJECT", position + 1)
+
+        var fragment: Fragment = CharactersFragment()
+
+        when (position) {
+            0 -> {
+                fragment = CharactersFragment()
+            }
+            1 -> {
+                fragment = StoriesFragment()
+            }
+            2 -> {
+                fragment = EventsFragment()
+            }
+            3 -> {
+                fragment = FavoritesFragment()
+            }
         }
 
         return fragment
