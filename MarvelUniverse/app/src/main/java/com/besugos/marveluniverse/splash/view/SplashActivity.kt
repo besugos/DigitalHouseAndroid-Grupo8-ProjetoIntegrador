5929d2d970cae9f3d02b22439b23432edb0cd96f
Wendel Bandeira <besugos@gmail.com>
2021-01-16 18:42:53 -0300

    Splash verifica se há usuário logado, e direciona o fluxo

--- MarvelUniverse/app/src/main/java/com/besugos/marveluniverse/splash/view/SplashActivity.kt
+++ MarvelUniverse/app/src/main/java/com/besugos/marveluniverse/splash/view/SplashActivity.kt
@@ -1,23 +1,38 @@
 package com.besugos.marveluniverse.splash.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.besugos.marveluniverse.login.view.LoginActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.besugos.marveluniverse.MainActivity
 import com.besugos.marveluniverse.R
+import com.besugos.marveluniverse.login.view.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_splash)
 
+        auth = FirebaseAuth.getInstance()
+
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+
+            if (auth.currentUser == null) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }, 2000)
     }
 }
