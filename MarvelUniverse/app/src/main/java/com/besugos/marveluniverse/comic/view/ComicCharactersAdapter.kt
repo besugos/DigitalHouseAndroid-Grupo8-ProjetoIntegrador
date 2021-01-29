125904bb23f734e13c349aebadf7923f574b0e97
gabrielaraujoz <garaujo.eng@gmail.com>
2021-01-28 21:09:31 -0300

    Comic recyclerviews ok

---
+++ MarvelUniverse/app/src/main/java/com/besugos/marveluniverse/comic/view/ComicCharactersAdapter.kt
@@ -0,0 +1,22 @@
+package com.besugos.marveluniverse.comic.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.besugos.marveluniverse.R
+import com.besugos.marveluniverse.home.model.CharacterSummaryModel
+
+class ComicCharactersAdapter (private var listCharacters: MutableList<CharacterSummaryModel>): RecyclerView.Adapter<ComicCharactersViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComicCharactersViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_view_details, parent, false)
+        return ComicCharactersViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ComicCharactersViewHolder, position: Int) {
+        val event = listCharacters[position]
+        holder.bind(event)
+    }
+
+    override fun getItemCount() = listCharacters.size
+
+}
---
+++ MarvelUniverse/app/src/main/java/com/besugos/marveluniverse/comic/view/ComicEventsAdapter.kt
@@ -0,0 +1,23 @@
+package com.besugos.marveluniverse.comic.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.besugos.marveluniverse.R
+import com.besugos.marveluniverse.character.view.CharactersEventsViewHolder
+import com.besugos.marveluniverse.home.model.EventSummaryModel
+
+class ComicEventsAdapter (private var listEvents: MutableList<EventSummaryModel>): RecyclerView.Adapter<ComicEventsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComicEventsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_view_details, parent, false)
+        return ComicEventsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ComicEventsViewHolder, position: Int) {
+        val event = listEvents[position]
+        holder.bind(event)
+    }
+
+    override fun getItemCount() = listEvents.size
+
+}
